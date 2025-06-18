0759eaa901daea3beacc87b19cd58c27674ff724
Vasilieva Kira <kirazvasili@gmail.com>
2025-06-18 13:40:10 +0400

    4

--- sprint_1/4-functions/functions.js
+++ sprint_1/4-functions/functions.js
@@ -5,6 +5,10 @@
 
 // Код задания 1
 
+function greet(name){
+   console.log(`Привет, ${name}!`);
+}
+greet("Вася");
 
 /** Задание 2.
  * Перепиши функцию `multiply` в виде стрелочной функции.
@@ -15,6 +19,9 @@
 
 // Код задания 2
 
+const multiply = (a, b) => a * b;
+console.log(multiply(3,5))
+
 /**
  * Задание 3.
  Создай стрелочную функцию sum, которая принимает два числа и возвращает их сумму.
@@ -22,7 +29,8 @@
 
 // Код задания 3
 
-
+const sum = (a, b) => a + b;
+console.log(sum(9, 3));
 
 /**
  * Задание 4.
@@ -31,6 +39,12 @@
 
 // Код задания 4
 
+function isEven(num){
+   if(num % 2 == 0) return true;
+   else return false;
+}
+console.log(isEven(3));
+console.log(isEven(8));
 
 /**
  * Задание 5.
@@ -41,6 +55,10 @@
 
 // Код задания 5
 
+function formatName(firstName, lastName){
+   return `${lastName} ${firstName[0]}.`
+}
+console.log(formatName("Иван", "Иванов"));
 
 /**
  * Задание 6.
@@ -53,12 +71,37 @@
 
 // Код задания 6
 
+function translateStatus(code){
+   switch(code){
+      case "success":
+         return "Успешно";
+      case "error":
+         return "Ошибка";
+      case "pending":
+         return "В ожидании";
+      default:
+         return "Неизвестно";
+   }
+}
+console.log(translateStatus("success"));
+//console.log(translateStatus("error"));
+//console.log(translateStatus("pending"));
+//console.log(translateStatus("lol"));
 
 /**
  * Задание 7.
  Функция canLogin(user) возвращает true, если user.isActive === true и user.role === 'admin'.
  **/
 // Код задания 7
+
+function canLogin(user){
+   if(user.isActive === true && user.role === "admin") return true;
+   else return false;
+}
+let userOne = { isActive: true, role: "admin"};
+let userTwo = { isActive: true, role: "user"};
+console.log(canLogin(userOne));
+console.log(canLogin(userTwo));
 
 /**
  * Задание 8.
@@ -71,3 +114,6 @@
  **/
 
 // Код задания 8
+
+const showArgs = (...arguments) => console.log(arguments);
+showArgs(1, 2, 3);
