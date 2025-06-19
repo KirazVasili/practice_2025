41e8925c5a6350b8587e15c5d3fca77693468b78
Vasilieva Kira <kirazvasili@gmail.com>
2025-06-19 21:09:38 +0400

    7

--- sprint_1/7-asynchronous-code/asynchronous-code.js
+++ sprint_1/7-asynchronous-code/asynchronous-code.js
@@ -8,6 +8,10 @@
 
 // Код задания 1
 
+function delayLog(msg, ms) {
+    setTimeout(() => {console.log(msg);}, ms);
+}
+delayLog("Привет", 1000);
 
 /**
  * Задание 2.
@@ -16,6 +20,12 @@
 
 // Код задания 2
 
+function sequentialLogs(){
+    setTimeout(() => {console.log(1);}, 1000);
+    setTimeout(() => {console.log(2);}, 2000);
+    setTimeout(() => {console.log(3);}, 3000);
+}
+sequentialLogs();
 
 /**
  * Задание 3.
@@ -25,6 +35,14 @@
 
 // Код задания 3
 
+function checkNumber(num){
+    return new Promise(() => {
+        if(num>10) console.log("OK");
+        else console.log("Too small");
+    });
+}
+checkNumber(4);
+
 /**
  * Задание 4.
  * Напиши функцию `fakeFetch(url)`, которая возвращает промис.
@@ -33,6 +51,12 @@
 
 // Код задания 4
 
+function fakeFetch(url){
+    return new Promise(() => {
+        setTimeout(() => {console.log(`${url} loaded`)});
+    });
+}
+fakeFetch("www.fake_url.com")
 
 /**
  * Задание 5.
@@ -45,6 +69,16 @@
 
 // Код задания 5
 
+function promise(num){
+    return new Promise((resolve) => {
+        resolve(num *= 2);
+    }).then(function(num){
+        return num += 10;
+    }).then(function(num){
+        console.log(num /= 3);
+    });
+}
+promise(5);
 
 /**
  * Задание 6.
@@ -54,6 +88,15 @@
 
 // Код задания 6
 
+async function loadData(url){
+    try{
+        const data = await fakeFetch(url);
+        console.log(data);
+    }catch(error){
+        console.error("error");
+    }
+}
+loadData("url_fake.net")
 
 /**
  * Задание 7.
@@ -63,6 +106,17 @@
 
 // Код задания 7
 
+const fetch = (url) => new Promise(resolve => resolve(url));
+
+async function loadAll(urls) {
+    try{
+        const data = await Promise.all([fetch(urls[0]), fetch(urls[1]), fetch(urls[2])]);
+        console.log(data);
+    }catch(error){
+        console.error("error");
+    }
+}
+loadAll(["url1", "url2", "url3"]);
 
 /**
  * Задание 8.
@@ -72,6 +126,15 @@
 
 // Код задания 7
 
+async function loadFirst(urls) {
+    try{
+        const data = await Promise.race([fetch(urls[0]), fetch(urls[1]), fetch(urls[2])]);
+        console.log(`Победил - ${data}`);
+    }catch(error){
+        console.error("error");
+    }
+}
+loadFirst(["url1", "url2", "url3"]);
 
 /**
  * Задание 9.
@@ -80,6 +143,20 @@
 
 // Код задания 9
 
+async function returnData(url) {
+    try{
+        const data = await fetch(url);
+        console.log(`Возвращен - ${data}`);
+    }catch(error){
+        console.error("error");
+    }
+}
+function processSequentially(arr, asyncFn){
+    for(let item of arr){
+        asyncFn(item);
+    }
+}
+processSequentially(["data1", "data2", "data3"], returnData)
 
 /**
  * Задание 10.
@@ -87,3 +164,10 @@
  **/
 
 // Код задания 10
+
+function sleep(ms){
+    return new Promise((resolve) => {
+        setTimeout(() => {resolve(console.log("Промис вернулся"));}, ms)
+    });
+}
+sleep(3000);
